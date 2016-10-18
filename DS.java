ec539ff1ec8168c5b88e6fcc331e187ad87b4c54
AR <aish2012@gmail.com>
2016-10-18 16:42:33 -0400

    Testing HM, ArrayLists

---
+++ DS.java
@@ -0,0 +1,54 @@
+/* package whatever; // don't place package name! */
+
+import java.io.*;
+import java.util.*;
+
+class DSA
+{
+    public HashMap<String, Integer> nameAgeMap(String[] people, Integer[] ages){
+        HashMap<String, Integer> hmap = new HashMap<String, Integer>();
+        if (people.length != ages.length) { 
+            return null;
+        }
+        
+        for (int i = 0; i < people.length; i++){
+            hmap.put(people[i], ages[i]);           
+        }
+        return hmap;        
+    }
+
+    public ArrayList<String> strArrToArrLst (String[] group){
+        ArrayList<String> arrlst = new ArrayList<String>();
+        for(String s: group){
+            arrlst.add(s);
+        }
+        return arrlst;
+
+    }
+    
+    
+    
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        DSA test = new DSA();
+        //String[] ppl = new String[3];
+        String[] ppl = {"Bruce", "Shane", "Aibar"};
+        
+        //Integer[] age = new Integer[10];
+        Integer[] age = {22, 21, 23};
+        
+        HashMap<String, Integer> nameAge = test.nameAgeMap(ppl, age);
+        
+        for (String s: nameAge.keySet()){
+            System.out.println("Key : " + s + "      Value: " + nameAge.get(s));
+        }
+
+        ArrayList<String> tkdArrLst = test.strArrToArrLst(ppl);
+        for (String s: tkdArrLst){
+            System.out.println(" Array List element is: " + s);
+        }
+
+
+
+    }
+}
